5d09eddd865d4622453d6d15c0154c1e55677428
SravaniValueLabs <sravanivaluelabs@gmail.com>
2015-12-18 18:51:47 +0530

    Update Blockly1.js
    @auto-deploy

--- scripts/Deploy/Blockly1.js
+++ scripts/Deploy/Blockly1.js
@@ -5,4 +5,6 @@ execute=authenticated
   **/ 
  /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_tweet" id="1" inline="true" x="223" y="50"><value name="tweetString"><block type="text" id="2"><field name="TEXT">Hello Sravani. Testing the deployment!!!</field></block></value></block></xml>*#*#*/
 tweet ('Hello Sravani. Testing the deployment!!!');
+
+//edit
 			
