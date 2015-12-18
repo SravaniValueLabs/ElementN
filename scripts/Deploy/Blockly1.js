db9115632b6e2a8c725e8301c5e8f1a500b8d6ef
SravaniValueLabs <sravanivaluelabs@gmail.com>
2015-12-18 18:50:17 +0530

    1

---
+++ scripts/Deploy/Blockly1.js
@@ -0,0 +1,8 @@
+/** Script ACLs do not delete 
+ read=nobody 
+write=nobody
+execute=authenticated 
+  **/ 
+ /*#*beginblockly*#*<xml xmlns="http://www.w3.org/1999/xhtml"><block type="scriptr_tweet" id="1" inline="true" x="223" y="50"><value name="tweetString"><block type="text" id="2"><field name="TEXT">Hello Sravani. Testing the deployment!!!</field></block></value></block></xml>*#*#*/
+tweet ('Hello Sravani. Testing the deployment!!!');
+			
